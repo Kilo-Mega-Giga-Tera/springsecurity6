d264263cb8582375f0ecacf04b090eccc2c0c68a
AHJ <superfantastic@kakao.com>
2024-06-12 00:32:50 +0900

    csrf

--- src/main/java/com/app/security/controller/ContactController.java
+++ src/main/java/com/app/security/controller/ContactController.java
@@ -5,7 +5,7 @@ import com.app.security.repository.ContactMessageRespository;
 import com.app.security.utils.CommonUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
@@ -17,7 +17,7 @@ public class ContactController {
     private final ContactMessageRespository contactMessageRespository;
 
     @PostMapping("/contact")
-    public ContactMessages saveContactInquiryDetails(@RequestParam("id") ContactMessages contactMessage) {
+    public ContactMessages saveContactInquiryDetails(@RequestBody ContactMessages contactMessage) {
         contactMessage.setContactId(CommonUtils.getServiceReqNumber());
         contactMessage.setCreateDt(new Date(System.currentTimeMillis()));
         return contactMessageRespository.save(contactMessage);
