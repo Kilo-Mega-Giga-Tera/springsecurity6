ac79f94bc1ec398996819251b1e7c7f42162f4a4
AHJ <superfantastic@kakao.com>
2024-06-17 00:10:42 +0900

    preFilter sample

--- src/main/java/com/app/security/controller/ContactController.java
+++ src/main/java/com/app/security/controller/ContactController.java
@@ -4,11 +4,15 @@ import com.app.security.model.ContactMessages;
 import com.app.security.repository.ContactMessageRespository;
 import com.app.security.utils.CommonUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreFilter;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,11 +20,30 @@ public class ContactController {
 
     private final ContactMessageRespository contactMessageRespository;
 
+    /*
+     *  This code is test for method level security.
+     *  If 'contactName' contains the word 'Test', it will be filtered.
+     *
+     *  Check before the method is executed.
+     */
+    @PreFilter("filterObject.contactName != 'Test'")
     @PostMapping("/contact")
-    public ContactMessages saveContactInquiryDetails(@RequestBody ContactMessages contactMessage) {
-        contactMessage.setContactId(CommonUtils.getServiceReqNumber());
-        contactMessage.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactMessageRespository.save(contactMessage);
+    public List<ContactMessages> saveContactInquiryDetails(@RequestBody List<ContactMessages> contactMessage) {
+
+        if (ObjectUtils.isEmpty(contactMessage))
+            throw new RuntimeException("Cannot include Test");
+
+        List<ContactMessages> contactMessageList = new ArrayList<>();
+        ContactMessages message = contactMessage.get(0);
+
+        message.setContactId(CommonUtils.getServiceReqNumber());
+        message.setCreateDt(new Date(System.currentTimeMillis()));
+
+        message = contactMessageRespository.save(message);
+
+        contactMessageList.add(message);
+
+        return contactMessageList;
     }
 
 }
